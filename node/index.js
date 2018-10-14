d37c9b224b0fb47990bc9659a41d93fa7b4cbd15
yangming <472102644@qq.com>
2018-10-14 14:55:18 +0800

    fix:request

--- node/index.js
+++ node/index.js
@@ -4,7 +4,7 @@
 
 const http = require('http')
 const axios = require('axios')
-const token = '14_9Fzm8-PigyNupNE88aLCfUD1Bl12LocVky8flnUaeDSSSnlmbu6fQxj-sx9daMiYNezv4xuYIMsgccptIze9--mNLBhkaGxHvnqAvTBnhKk-7Rv0nUcxero6xT3CBrnjJTv5bfvUVlus-1AQECFjAHAYWZ'
+// const token = '14_9Fzm8-PigyNupNE88aLCfUD1Bl12LocVky8flnUaeDSSSnlmbu6fQxj-sx9daMiYNezv4xuYIMsgccptIze9--mNLBhkaGxHvnqAvTBnhKk-7Rv0nUcxero6xT3CBrnjJTv5bfvUVlus-1AQECFjAHAYWZ'
 const querystring = require('querystring')
 
 let getTokenStartTime
@@ -15,7 +15,7 @@ const getAccessToken = () => {
     console.log('从缓存取token')
     return Promise.resolve(accessToken)
   }
-  console.log('token 失效，重新获取')
+  console.log('获取token')
   // token 失效，需要重新获取
   const url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx33160dea62feba36&secret=20304416114d0da2a81bc8211462fe36`
   return axios.get(url).then(res => {
@@ -26,8 +26,12 @@ const getAccessToken = () => {
   })
 }
 
+/**
+ * deprecated
+ * @returns {Promise|*|c|PromiseLike<T | never>|Promise<T | never>}
+ */
 const addTemplate = () => {
-  const url = `https://api.weixin.qq.com/cgi-bin/wxopen/template/add?access_token=${token}`
+  const url = `https://api.weixin.qq.com/cgi-bin/wxopen/template/add?access_token=`
   console.log(url)
   return axios.post(url, {
     id: '承诺状态通知',
@@ -38,30 +42,34 @@ const addTemplate = () => {
 }
 
 const sendTemplateMessage = (reqUrl) => {
+  if (!accessToken) {
+    throw new Error('请先获取token，/api/getAccessToken')
+  }
+
   const url = `https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=${accessToken}`
   const templateId = 'keRZcuEsnmNUVh6I2qd9isd2MOLM7LHAmry0RwURZ9k'
 
   const search = reqUrl.substring(reqUrl.indexOf('?') + 1)
   const params = querystring.parse(search)
 
-  console.log(url)
-  console.log({
-    access_token: accessToken || token,
-    touser: 'okbFo5JofpMsNWsD156pnLpo5yXc',
-    template_id: templateId,
-    form_id: params.formId
-  })
-
   return axios.post(url, {
-    access_token: accessToken || token,
-    touser: 'okbFo5JofpMsNWsD156pnLpo5yXc',
-    template_id: templateId,
+    access_token: accessToken,
+    touser: params.openId,
+    template_id: params.templateId || templateId,
     form_id: params.formId,
+    page: params.page,
     data: {
       keyword1: {
-        value: 339208499
+        value: params.value1
+      },
+      keyword2: {
+        value: params.value2
+      },
+      keyword3: {
+        value: params.value3
       }
-    }
+    },
+    emphasis_keyword: 'keyword1.DATA'
   })
 }
 
