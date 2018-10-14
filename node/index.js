aa028f72f62cbdc5beec8a981493cd6c00385141
yangming <472102644@qq.com>
2018-10-14 16:52:51 +0800

    debug

--- node/index.js
+++ node/index.js
@@ -87,9 +87,11 @@ const server = http.createServer((req, res) => {
   } else if (/\/sendTemplateMessage/.test(req.url)) {
     sendTemplateMessage(req.url).then(data => {
       res.writeHead(200)
+      console.log('发送消息成功', data)
       res.end(JSON.stringify(data))
     }).catch(data => {
       res.writeHead(200)
+      console.log('发送消息失败', data)
       res.end(JSON.stringify(data))
     })
   } else if (req.url === '/addTemplate') {
