07e60037db33b5ac44bad9213d7c7cc58d947dc0
杨明 <yangm@shinemo.com>
2022-11-05 16:15:05 +0800

    debug

--- node/db.js
+++ node/db.js
@@ -6,12 +6,13 @@ const appKey = 'OR3zEynwWJ7f8bk95AdiGFzJ'
 const serverURLs = 'https://api.iming.work'
 AV.init({ appId, appKey, serverURLs })
 
-function getAv (openid, type) {
+async function getAv (openid, type) {
   const instance = new Query('CardBag')
   instance.equalTo('type', type)
   instance.equalTo('openid', openid)
   instance.descending('createdAt')
-  return instance.find()
+  const res = await instance.find()
+  return res.map(x => x.toJSON())
 }
 
 module.exports = {
